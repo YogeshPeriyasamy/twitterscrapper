55e790653544783f593f61b30aad1acc8d0eb65c
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2025-05-13 14:57:44 +0530

    recommit

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App'; // Your main App component
-// import './styles.css'; // Global styles
-
-// // Render the App component into the DOM
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root') // The root element in the HTML
-// );
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
